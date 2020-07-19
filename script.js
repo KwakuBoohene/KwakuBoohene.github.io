9523311e49e9d9d68083de024dc106d33abba1a1
KwakuBoohene <39279771+KwakuBoohene@users.noreply.github.com>
2020-07-19 10:07:25 +0000

    navbar set

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
 $(document).ready(function () {
-$(".sidebar-content > div > a").click(function(e) {
-    console.log(this.id)     
+$(".sidebar-content > div > a").click(function(e) {    
      gTScroll(this.id)
+     ToggleDisplay()
  });
+
+$("#navToggle").click(function(e){
+     ToggleDisplay()
+}) 
+
+
 })
 
-function gTScroll(id) {
-     
+function gTScroll(id) {  
      $('html, body').animate({
           scrollTop: $("div."+id).offset().top
      }, 1000)
+ }
 
+ function ToggleDisplay(){
+
+     var side = ($(".sidebar-content").css("display"));
+     var winwidth = $(window).width()
+     
+      if( side != "none" && winwidth <=1024){
+          $(".sidebar-content").hide();
+      }else{
+           $(".sidebar-content").show();
+      }
  }
